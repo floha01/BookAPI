54e9148f6910e836f6c0c508b8fbc0768166a67c
Flo <flo.hartl@gmx.net>
2025-12-10 12:22:44 +0100

    Initial commit

---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Data;
+using ProductApi.Models;
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //TODO: CRUD
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpGet]
+        public IActionResult GetProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product.Description);
+        }
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return NoContent();
+        }
+        [HttpPut]
+        public IActionResult UpdateProduct(int id, Product product)
+        {
+            var p = _context.Products.Find(id);
+            if (p == null)
+            {
+                return NotFound();
+            }
+            p.Title = product.Title;
+            p.Description = product.Description;
+            _context.SaveChanges();
+            return Ok(_context.Products.ToList());
+        }
+
+
+    }
+}
